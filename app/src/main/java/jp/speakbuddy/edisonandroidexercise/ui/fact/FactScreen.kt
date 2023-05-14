43b88b060cf9c2efbbdd4b0ff0a56e9a9abef1be
Caique <88202936+CaiqueMovefast@users.noreply.github.com>
2023-05-14 11:56:06 +0900

    UI improvements and changes

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/FactScreen.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/FactScreen.kt
@@ -2,8 +2,10 @@ package jp.speakbuddy.edisonandroidexercise.ui.fact
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -12,8 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import jp.speakbuddy.edisonandroidexercise.R
 import jp.speakbuddy.edisonandroidexercise.ui.theme.EdisonAndroidExerciseTheme
 import org.koin.androidx.compose.getViewModel
 
@@ -21,6 +25,8 @@ import org.koin.androidx.compose.getViewModel
 fun FactScreen() {
     val viewModel: FactViewModel = getViewModel()
     val fact = viewModel.fact.observeAsState()
+    val length = viewModel.length.observeAsState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -32,30 +38,66 @@ fun FactScreen() {
             alignment = Alignment.CenterVertically
         )
     ) {
-        Text(
-            text = "Fact",
-            style = MaterialTheme.typography.titleLarge
-        )
-
-        Text(
-            text = fact.value ?: "",
-            style = MaterialTheme.typography.bodyLarge
-        )
-
-        val onClick = {
+        FactView(fact = fact.value ?: "")
+        LengthView(length = length.value ?: 0)
+        UpdateFactButton(onClick = {
             viewModel.updateFact()
-        }
+        })
+    }
+}
 
-        Button(onClick = onClick) {
-            Text(text = "Update fact")
+@Composable
+fun FactView(fact: String) {
+    Text(
+        text = stringResource(id = R.string.fact),
+        style = MaterialTheme.typography.titleLarge
+    )
+
+    if (fact.contains(stringResource(id = R.string.cats), ignoreCase = true)) {
+        Text(
+            text = stringResource(id = R.string.multiple_cats),
+            style = MaterialTheme.typography.titleMedium
+        )
+    }
+
+    Text(
+        text = fact,
+        style = MaterialTheme.typography.bodyLarge
+    )
+}
+
+@Composable
+fun LengthView(length: Int) {
+    if (length >= 100) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 16.dp)) {
+            Text(
+                text = stringResource(id = R.string.lbl_length, length.toString()),
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
         }
     }
 }
 
-@Preview
+@Composable
+fun UpdateFactButton(onClick: (Unit) -> Unit) {
+    Button(onClick = { onClick(Unit) }) {
+        Text(text = stringResource(id = R.string.update_fact))
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 private fun FactScreenPreview() {
     EdisonAndroidExerciseTheme {
-        FactScreen()
+        Column(
+            Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            FactView(fact = "Lorem ipsum dolor sit cats, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+            LengthView(length = 120)
+            UpdateFactButton(onClick = {})
+        }
     }
 }
